dddf664ab103d9c56c57921181071983a8b20509
sukyung <jellosu0@gmail.com>
2018-03-29 17:29:26 +0000

    first looto

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @numlists=[1,65,73,53,24,656,34,733] .sample(3).sort
     
-    0<@numlist<46 .sample(7).sort
-      
-    end
+    @num=(1..45).to_a .sample(7).sort
   end
+end
